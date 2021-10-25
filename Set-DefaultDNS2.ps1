df0ef787abaec0148e85addc93a14211e232f973
654321bear <49491804+654321bear@users.noreply.github.com>
2021-10-25 15:55:25 -0500

    Add files via upload

---
+++ Set-DefaultDNS2.ps1
@@ -0,0 +1,7 @@
+﻿$DefDNS = "172.20.100.1, 172.20.100.2, 172.20.100.3"$pc_list = get-content "C:\temp\ServerDNS.txt"
+
+$NICs = Get-WMIObject Win32_NetworkAdapterConfiguration -computername $pc_list |where{$_.IPEnabled -eq “TRUE”}
+  Foreach($NIC in $NICs) {
+ $NIC.SetDNSServerSearchOrder($DefDNS)
+ $NIC.SetDynamicDNSRegistration(“TRUE”)
+}
